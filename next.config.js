9d330a2de552b488ede0ff017fe17e5c150f4fe8
Adwin <adwin.dwitaufani@icreon.com>
2022-06-10 13:52:21 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['http://localhost:8888/wordpress/graphql'],
+  },  
 }
 
 module.exports = nextConfig
